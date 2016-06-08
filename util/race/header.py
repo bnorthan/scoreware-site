96bda47fc929a50b6d84aa413097e394a2037452
Brian Northan <bnorthan@gmail.com>
2016-06-08 06:32:17 -0400

    updates

---
+++ util/race/header.py
@@ -0,0 +1,17 @@
+class RaceHeader:
+    headers={}
+    headers['place']=['place']
+    headers['fname']=['first_name', 'first name','fname','first']
+    headers['lname']=['last_name','last name','lname','last']
+    headers['gender']=['gender','sex']
+    headers['age']=['age']
+    headers['city']=['city']
+    headers['state']=['state','st']
+    headers['div_place']=['div place']
+    headers['gender_place']=['gender Place']
+    headers['gun_time']=['gtime', 'time', 'guntime', 'gun_time']
+    headers['pace']=['pace']
+    headers['hmrrc']=['member','HMRRC Event']
+
+    
+
